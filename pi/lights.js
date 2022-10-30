2646f88fb3d273b0f0534e982d6859ac63e6688a
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-30 16:27:01 -0400

    more

--- pi/lights.js
+++ pi/lights.js
@@ -8,7 +8,7 @@ module.exports = {
             } else {
                 // Write reverse status to pin... gotta figure this one out later
                 // TOOD: Why do i need to reverse the status?
-                gpio.write(26, !status, (err) => {
+                gpio.write(26, true, (err) => {
                     if(err) {
                         console.log(err);
                     }
--- pi/sounds.js
+++ pi/sounds.js
@@ -3,7 +3,9 @@ const { exec } = require('child_process');
 module.exports = {
     playSound: () => {
         exec("curl http://192.168.0.161:3000/sound", (error) => {
-            console.log('error', error.message);
+            if(error.message) {
+                console.log('error', error.message);
+            }
         });
     }
 }
