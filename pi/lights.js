302a268d2270a7bf3d8e70ee3820824e019dc7cb
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-30 16:39:45 -0400

    logging

--- pi/lights.js
+++ pi/lights.js
@@ -2,12 +2,15 @@ const gpio = require('rpi-gpio');
 
 module.exports = {
     playLights: () => {
+        console.log('playing lights');
         gpio.setup(26, gpio.DIR_OUT, (err) => {
+            console.log('setting up 26')
             if(err) {
                console.log(err)
             } else {
                 // Write reverse status to pin... gotta figure this one out later
                 // TOOD: Why do i need to reverse the status?
+                console.log('writing to 26');
                 gpio.write(26, true, (err) => {
                     if(err) {
                         console.log(err);
