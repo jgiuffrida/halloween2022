0d105a5dda3ae5c1e5c507930106afcc9f916f4b
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-30 17:50:00 -0400

    more

--- pi/lights.js
+++ pi/lights.js
@@ -3,7 +3,7 @@ const gpio = require('rpi-gpio');
 module.exports = {
     playLights: () => {
         console.log('playing lights');
-        gpio.setup(26, gpio.DIR_OUT, (err) => {
+        gpio.setup(16, gpio.DIR_OUT, (err) => {
             console.log('setting up 26')
             if(err) {
                console.log(err)
@@ -11,10 +11,18 @@ module.exports = {
                 // Write reverse status to pin... gotta figure this one out later
                 // TOOD: Why do i need to reverse the status?
                 console.log('writing to 26');
-                gpio.write(26, false, (err) => {
+                gpio.write(16, false, (err) => {
                     if(err) {
                         console.log(err);
                     }
+
+                    setTimeout(() => {
+                        gpio.write(16, true, (err) => {
+                            if(err) {
+                                console.log(err);
+                            }
+                        });
+                    }, 5000);
                 });
             }
         });
