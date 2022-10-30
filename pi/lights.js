03faa34cc49389a548e0c48397d29cd1cc17367b
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-30 18:17:37 -0400

    use 18

--- pi/lights.js
+++ pi/lights.js
@@ -3,21 +3,21 @@ const gpio = require('rpi-gpio');
 module.exports = {
     playLights: () => {
         console.log('playing lights');
-        gpio.setup(16, gpio.DIR_OUT, (err) => {
-            console.log('setting up 26')
+        gpio.setup(18, gpio.DIR_OUT, (err) => {
+            console.log('setting up 18')
             if(err) {
                console.log(err)
             } else {
                 // Write reverse status to pin... gotta figure this one out later
                 // TOOD: Why do i need to reverse the status?
-                console.log('writing to 26');
-                gpio.write(16, false, (err) => {
+                console.log('writing to 18');
+                gpio.write(18, false, (err) => {
                     if(err) {
                         console.log(err);
                     }
 
                     setTimeout(() => {
-                        gpio.write(16, true, (err) => {
+                        gpio.write(18, true, (err) => {
                             if(err) {
                                 console.log(err);
                             }
