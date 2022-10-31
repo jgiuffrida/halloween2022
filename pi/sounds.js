6d5c360a57dfab681ec0db23099908a4553ad7af
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-31 08:40:09 -0400

    more

--- pi/sounds.js
+++ pi/sounds.js
@@ -3,7 +3,7 @@ const { exec } = require('child_process');
 module.exports = {
     playSound: () => {
         exec("curl http://192.168.0.161:3000/sound", (error) => {
-            if(error.message) {
+            if(error && error.message) {
                 console.log('error', error.message);
             }
         });
