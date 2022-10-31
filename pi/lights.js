4b8768126e2ec0af85cf53fb6c1714d35d62956d
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-31 08:11:48 -0400

    do a sequence

--- pi/lights.js
+++ pi/lights.js
@@ -1,30 +1,54 @@
 const gpio = require('rpi-gpio');
 
+const PIN = 12;
+
+const writePin = (value, cb) => {
+    gpio.write(PIN, value, (err) => {
+        if(err) {
+            console.log(`error writing ${value.toString()} to ${PIN}`);
+        } else {
+            cb(value);
+        }
+    })
+}
+
+const onOffOn = (cb) => {
+ writePin(true, () => {
+    setTimeout(() => {
+        writePin(false, () => {
+            setTimeout(() => {
+                writePin(true, cb);
+            }, 200)
+        })
+    }, 250);
+ });
+}
+
 module.exports = {
     playLights: () => {
         console.log('playing lights');
-        gpio.setup(12, gpio.DIR_OUT, (err) => {
-            console.log('setting up 18')
-            if(err) {
-               console.log(err)
-            } else {
-                // Write reverse status to pin... gotta figure this one out later
-                // TOOD: Why do i need to reverse the status?
-                console.log('writing to 18');
-                gpio.write(12, false, (err) => {
-                    if(err) {
-                        console.log(err);
-                    }
 
+        gpio.setup(PIN, gpio.DIR_OUT, (err) => {
+            setTimeout(() => {
+                onOffOn(() => {
                     setTimeout(() => {
-                        gpio.write(12, true, (err) => {
-                            if(err) {
-                                console.log(err);
-                            }
+                        onOffOn(() => {
+                            setTimeout(() => {
+                                onOffOn(() => {
+                                    setTimeout(() => {
+                                        onOffOn(() => {
+                                            setTimeout(() => {
+                                                writePin(false);
+                                            });
+                                        })
+                                    }, 6000)
+                                })
+                            }, 4000)
                         });
-                    }, 5000);
-                });
-            }
+                    }, 2000)
+                })
+            }, 4000); // don't start flashing until the stream comes on
         });
+    
     }
 }
