4f23f958028d0688ae32b62929f945d73bd609ec
Joe Giuffrida <jm.giuffrida@gmail.com>
2022-10-31 08:25:13 -0400

    more

--- pi/lights.js
+++ pi/lights.js
@@ -4,7 +4,7 @@ const PIN = 12;
 
 const writePin = (value, cb) => {
     gpio.write(PIN, value, (err) => {
-        if(err) {
+        if (err) {
             console.log(`error writing ${value.toString()} to ${PIN}`);
         } else {
             cb(value);
@@ -13,21 +13,20 @@ const writePin = (value, cb) => {
 }
 
 const onOffOn = (cb) => {
- writePin(true, () => {
-    setTimeout(() => {
-        writePin(false, () => {
-            setTimeout(() => {
-                writePin(true, cb);
-            }, 200)
-        })
-    }, 250);
- });
+    writePin(true, () => {
+        setTimeout(() => {
+            writePin(false, () => {
+                setTimeout(() => {
+                    writePin(true, cb);
+                }, 200)
+            })
+        }, 250);
+    });
 }
 
 module.exports = {
     playLights: () => {
         console.log('playing lights');
-
         gpio.setup(PIN, gpio.DIR_OUT, (err) => {
             setTimeout(() => {
                 onOffOn(() => {
@@ -49,6 +48,5 @@ module.exports = {
                 })
             }, 4000); // don't start flashing until the stream comes on
         });
-    
     }
 }
